70e290949dd596d5227a003eae95e4ed29e6a5d6
bnadar-1987 <bavithra.s.nadar@gmail.com>
2020-06-29 00:55:47 -0700

    Django api view get

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-'''from rest_framework.views import APIView
+#from django.shortcuts import render
+from rest_framework.views import APIView
 from rest_framework.response import Response
 
 class HelloApiView(APIView):
@@ -13,7 +13,7 @@ class HelloApiView(APIView):
         'Is mapped manually to URLs'
         ]
         return Response({'message': 'Hello', 'an_apiview': an_apiview})
-        '''
+
 
 
 
